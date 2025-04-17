1936d3fd89f5aad383f32980f6f67935536c3c80
Lauiee <132264578+Lauiee@users.noreply.github.com>
2025-04-17 19:30:28 +0900

    refactor: 로또 번호 정렬 로직 뷰 계층으로 이동

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,6 +1,8 @@
 package lotto.view;
 
 import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import lotto.model.Lotto;
 import lotto.model.LottoResults;
@@ -16,7 +18,9 @@ public class OutputView {
 
     public void printLottos(Lottos lottos){
         for (Lotto lotto : lottos.getLottos()) {
-            System.out.println(lotto.getNumbers());
+            List<Integer> lottoNumbers = lotto.getNumbers();
+            Collections.sort(lottoNumbers);
+            System.out.println(lottoNumbers);
         }
     }
 
